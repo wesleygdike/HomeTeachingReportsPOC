163f8b8cbb16fafcab9ddef0363b0863a431c7ee
Santos-Jefferson <jefferson2004@gmail.com>
2017-05-31 23:03:18 -0300

    Jefferson's conflict x Spencer

--- app/src/main/java/com/example/wesleydike/hometeachingreportspoc/MainActivity.java
+++ app/src/main/java/com/example/wesleydike/hometeachingreportspoc/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //test Jefferson
         //test Spencer
+        //test conflict!!Jefferson
     }
 }
