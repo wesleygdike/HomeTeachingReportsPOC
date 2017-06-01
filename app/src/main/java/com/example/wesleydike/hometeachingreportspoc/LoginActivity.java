62d3c85f4a00758a46cdaefcfda14dbe5f1e16b1
Santos-Jefferson <jefferson2004@gmail.com>
2017-05-31 22:42:37 -0300

    Initial commit

--- app/src/main/java/com/example/wesleydike/hometeachingreportspoc/LoginActivity.java
+++ app/src/main/java/com/example/wesleydike/hometeachingreportspoc/LoginActivity.java
@@ -43,7 +43,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
-
+    //test jefferson again
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
